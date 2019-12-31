b2349201deb5a3079358a0ef7b14b697b5ddfd8b
邹明哲 <zoumingzhe@qq.com>
2019-12-31 16:06:36 +0800

    update align

--- align/include/align.h
+++ align/include/align.h
@@ -4,11 +4,14 @@
 typedef struct align1 {
 	I8  value1_i8;
 	I8  value2_i8;
+	I8  value3_i8;
 }align1;
 
 typedef struct align2 {
 	I8  value1_i8;
-	I16 value2_i16;
+	I8  value2_i8;
+	I8  value3_i8;
+	I16 value4_i16;
 }align2;
 
 typedef struct align3 {
@@ -22,5 +25,13 @@ typedef struct align4 {
 	I8  value2_i8;
 	I16 value3_i16;
 }align4;
+
+typedef struct align_store {
+	I16 value1_i16;
+	I16 value2_i16;
+	I32 value3_i32;
+	I16 value4_i16;
+	I16 value5_i16;
+}align_store;
 
 #endif
--- align/source/main.cpp
+++ align/source/main.cpp
@@ -10,16 +10,20 @@ int main()
 	align2 a2;
 	align3 a3;
 	align4 a4;
+	align_store align_store;
 
 	cout << endl;
 	cout << "size a1\t: " << sizeof(a1) << endl;
 	cout << "size a1.value1_i8\t: "  << sizeof(a1.value1_i8)  << endl;
 	cout << "size a1.value2_i8\t: "  << sizeof(a1.value2_i8)  << endl;
+	cout << "size a1.value3_i8\t: "  << sizeof(a1.value3_i8)  << endl;
 
 	cout << endl;
 	cout << "size a2\t: " << sizeof(a2) << endl;
 	cout << "size a2.value1_i8\t: "  << sizeof(a2.value1_i8)  << endl;
-	cout << "size a2.value2_i16\t: " << sizeof(a2.value2_i16) << endl;
+	cout << "size a2.value2_i8\t: "  << sizeof(a2.value2_i8)  << endl;
+	cout << "size a2.value3_i8\t: "  << sizeof(a2.value3_i8)  << endl;
+	cout << "size a2.value4_i16\t: " << sizeof(a2.value4_i16) << endl;
 
 	cout << endl;
 	cout << "size a3\t: " << sizeof(a3) << endl;
@@ -32,6 +36,14 @@ int main()
 	cout << "size a4.value1_i8\t: "  << sizeof(a4.value1_i8)  << endl;
 	cout << "size a4.value2_i8\t: "  << sizeof(a4.value2_i8)  << endl;
 	cout << "size a4.value3_i16\t: " << sizeof(a4.value3_i16) << endl;
+
+	cout << endl;
+	cout << "size align_store\t: " << sizeof(align_store) << endl;
+	cout << "size align_store.value1_i16\t: " << sizeof(align_store.value1_i16) << endl;
+	cout << "size align_store.value2_i16\t: " << sizeof(align_store.value2_i16) << endl;
+	cout << "size align_store.value3_i32\t: " << sizeof(align_store.value3_i32) << endl;
+	cout << "size align_store.value4_i16\t: " << sizeof(align_store.value4_i16) << endl;
+	cout << "size align_store.value5_i16\t: " << sizeof(align_store.value5_i16) << endl;
 
 	getchar();
 	return 0;
