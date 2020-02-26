d4ba25564c347747acd7a910568dad6eff6634b9
邹明哲 <zoumingzhe@qq.com>
2020-02-26 10:32:48 +0800

    Update test_LZ77.py

--- LZ77/src/test_LZ77.py
+++ LZ77/src/test_LZ77.py
@@ -1,24 +1,40 @@
+import os
 from LZ77 import LZ77Compressor
 
-filename = "input.txt"
-compressor = LZ77Compressor(window_size=20) # window_size is optional
+def filewrite(path, datas):
+   "写入二进制文件"
+   filehandle = open(path, "wb+")
+   filehandle.write(datas)
+   filehandle.close()
 
-#pack   = compressor.compress("..\\examples\\" + filename, verbose=False)
-pack   = compressor.compress("..\\examples\\" + filename, verbose=True)
-print(pack.tobytes())
-print(pack.length())
-print(pack)
+def check(path1, path2):
+   "检查源文件和解压文件内容"
+   handle1 = open(path1, "rb")
+   handle2 = open(path2, "rb")
+   result = (handle1.read() == handle2.read())
+   handle1.close()
+   handle2.close()
+   return result
 
-filehandle = open("..\\examples\\compressed\\" + filename, "wb+");
-filehandle.write(pack.tobytes());
-filehandle.close();
-        
-unpack = compressor.decompress("..\\examples\\compressed\\" + filename)
+def info(path1, path2):
+    size1 = os.path.getsize(path1)
+    size2 = os.path.getsize(path2)
+    dict = {'bytes1': size1, 'bytes2': size2}
+    return dict
 
-filehandle = open("..\\examples\\decompressed\\" + filename, "wb+");
-filehandle.write(unpack.tobytes());
-filehandle.close();
+window_size = 100
+filename = "input"
+filetype = ".txt"
+filepath1 = "..\\examples\\" + filename + filetype
+filepath2 = "..\\examples\\compressed\\" + filename + "_window_" + str(window_size) + filetype
+filepath3 = "..\\examples\\decompressed\\" + filename + filetype
+compressor = LZ77Compressor(window_size) # window_size is optional
 
-##print(unpack)
-##print(unpack == 'aacaacabcabaaac')
+filewrite(filepath2, compressor.compress(filepath1, verbose=False).tobytes())
+filewrite(filepath3, compressor.decompress(filepath2).tobytes())
+
+result = info(filepath1, filepath2)
+print(check(filepath1, filepath3), window_size, result['bytes1'], result['bytes2'],
+      result['bytes2'] / result['bytes1'] * 100)
+
 input("按回车（Enter）继续")
