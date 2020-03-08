c2dea1eeccfc121357a93140241c43b8643148e0
zoumingzhe <zoumingzhe@qq.com>
2020-03-08 20:59:01 +0800

    Update main.cpp

--- FastLZ/source/main.cpp
+++ FastLZ/source/main.cpp
@@ -5,14 +5,27 @@ using namespace std;
 
 int main()
 {
-	char dat1[1024] = "hello the world, my name is zoumz, are you ok? hello zoumz i'm fine hello the kunlun database";
+	char dat1[1024] = "hello the world, my name is zoumz, are you ok?"
+		"hello zoumz i'm fine hello the kunlun database"
+		"wowmsjhdhfdhifwhdisaw"
+		"dsakncdsaiucdosacnjiwns"
+		"sanxhzugsadkdmlosaidwqi"
+		"dwqoiowedndwfiuewnfid"
+		"saijiuxAOIXNOIozIASIWQSI"
+		"XJSABXIASNXISANCNSANKIKknxzsjxsu"
+		"xsaxusqidxsaixnsanxswiew"
+		"zXjiuasxniuwqieiw"
+		"zXjiuasxniuwqieiw"
+		"zXjiuasxniuwqieiw"
+		"zXjiuasxniuwqieiw"
+		"zXjiuasxniuwqieiw";
 	char dat2[1024] = { 0 };
 	char dat3[1024] = { 0 };
 	
 
 	int len1 = strlen(dat1);
-	//int len2 = fastlz_compress(dat1, len1, dat2);
-	int len2 = fastlz_compress_level(2, dat1, len1, dat2);
+	int len2 = fastlz_compress(dat1, len1, dat2);
+	//int len2 = fastlz_compress_level(2, dat1, len1, dat2);
 	int len3 = fastlz_decompress(dat2, len2, dat3, len1);
 	
 	cout << endl;
