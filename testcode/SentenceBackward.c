7a59e55eefe9870c9352eac37f5488e93ee88fe5
zoumingzhe <zoumingzhe@qq.com>
2020-04-04 01:11:55 +0800

    Update SentenceBackward.c

--- testcode/SentenceBackward.c
+++ testcode/SentenceBackward.c
@@ -1,3 +1,4 @@
+// https://www.nowcoder.com/practice/48b3cb4e3c694d9da5526e6255bb73c3?tpId=37&tqId=21236&tPage=1&rp=&ru=%2Fta%2Fhuawei&qru=%2Fta%2Fhuawei%2Fquestion-ranking
 #include <stdio.h>
 
 void sentence(char *str)
