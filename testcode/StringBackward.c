ec17c70d6012bf0bf42be02b73f9cae2a409d266
zoumingzhe <zoumingzhe@qq.com>
2020-04-04 01:11:37 +0800

    Update StringBackward.c

--- testcode/StringBackward.c
+++ testcode/StringBackward.c
@@ -1,6 +1,7 @@
+// https://www.nowcoder.com/practice/e45e078701ab4e4cb49393ae30f1bb04?tpId=37&tqId=21235&tPage=1&rp=&ru=/ta/huawei&qru=/ta/huawei/question-ranking
 #include <stdio.h>
 
-void string(char *str)
+void backward(char *str)
 {
     int  idx = 0;
     while(str[idx++] != '\0');
@@ -19,7 +20,7 @@ int main()
     
     while(gets(str))
     {
-        string(str);
+        backward(str);
     }
 
     return 0;
