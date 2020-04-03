55531c675e6a354a9dbf8985b2cc1b9cd50cf0d8
zoumingzhe <zoumingzhe@qq.com>
2020-04-04 01:11:45 +0800

    Update NumberBackward.c

--- testcode/NumberBackward.c
+++ testcode/NumberBackward.c
@@ -1,21 +1,32 @@
+// https://www.nowcoder.com/practice/ae809795fca34687a48b172186e3dafe?tpId=37&tqId=21234&tPage=1&rp=&ru=%2Fta%2Fhuawei&qru=%2Fta%2Fhuawei%2Fquestion-ranking
 #include <stdio.h>
 
-void number(long num)
+void backward(char *str)
 {
-    while (num > 0)
+    int  idx = 0;
+    while(str[idx++] != '\0');
+    idx--;
+    idx--;
+    while(idx >= 0)
     {
-        printf("%d", num % 10);
-        num /= 10;
+        printf("%c", str[idx--]);
     }
     printf("\n");
+}
+
+void tostring(long num, char *str)
+{
+    sprintf(str, "%lld", num);
 }
 
 int main()
 {
     long num;
+    char tmp[100];
 
-    scanf("%ld", &num);
-    number(num);
+    scanf("%lld", &num);
+    tostring(num, tmp);
+    backward(tmp);
 
     return 0;
 }
