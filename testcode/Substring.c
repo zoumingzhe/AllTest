798d7f19636b8f08712c811f3d3ffcd37ed22081
zoumingzhe <zoumingzhe@qq.com>
2020-04-04 12:48:09 +0800

    Update Substring.c

--- testcode/Substring.c
+++ testcode/Substring.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int substring(char *str1, char *str2)
+int substring(char* str1, char* str2)
 {
     int idx1 = 0;
     int idx2 = 0;
 
-    while(str1[idx1] != '\0')
+    while (str1[idx1] != '\0')
     {
         if (str1[idx1] == str2[idx2])
         {
@@ -16,27 +16,54 @@ int substring(char *str1, char *str2)
             idx1 -= idx2;
             idx2 = 0;
         }
-        
+
         if (str2[idx2] == '\0')
         {
-            return 1;
+            return (idx1 - idx2);
         }
         idx1++;
     }
     return 0;
 }
 
+int substring2(char* str1, char* str2)
+{
+    int idx1 = 0;
+    int idx2 = 0;
+
+    while (str1[idx1] != '\0')
+    {
+        if (str1[idx1] == str2[idx2])
+        {
+            idx2++;
+        }
+        else
+        {
+            idx1 -= idx2;
+            idx2 = 0;
+        }
+
+        if (str2[idx2] == '\0')
+        {
+            return (idx1 - idx2 + 1);
+        }
+        idx1++;
+    }
+    return -1;
+}
+
 int main(void)
 {
+    int off = 0;
     char  str1[1000];
     char  str2[1000];
 
     gets(str1);
     gets(str2);
 
-    if (substring(str1, str2))
+    if ((off = substring2(str1, str2)) >= 0)
     {
-        printf("OK");
+        printf("OK [%s]", str1 + off);
     }
     else
     {
