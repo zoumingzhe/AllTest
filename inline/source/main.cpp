0c9e5a411d57c8246cf81b45496951124d82bf35
邹明哲 <zoumingzhe@qq.com>
2020-04-16 19:20:40 +0800

    Update main.cpp

--- inline/source/main.cpp
+++ inline/source/main.cpp
@@ -14,15 +14,24 @@ inline static int ifunc(int a, int b)
 	return (a + b);
 }
 
-/* 调试模式下，按Ctrl+F11打开反汇编窗口。 */
+/* 项目属性页下，设置“C/C++”->“优化”->“内联函数扩展”。
+   调试模式下，按Ctrl+F11打开反汇编窗口。 */
 int main()
 {
-	int dret = dfunc(1, 2);
-	int cret = cfunc(3, 4);
-	int iret = ifunc(5, 6);
-	cout << "hello"  << endl;
+	int dret;
+	int cret;
+	int iret;
+
+	cout << "hello" << endl;
+
+	dret = dfunc(1, 2);
 	cout << dret << endl;
+
+	cret = cfunc(3, 4);
 	cout << cret << endl;
+
+	iret = ifunc(5, 6);
 	cout << iret << endl;
+
 	return 0;
 }
